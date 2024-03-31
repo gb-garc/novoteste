89e0d05f38bd08eac9589a35ffc85b186f7e79cd
gb-garc <165524994+gb-garc@users.noreply.github.com>
2024-03-31 10:38:31 -0300

    Update app1_controle_funcionarios.py

--- app1_controle_funcionarios.py
+++ app1_controle_funcionarios.py
@@ -4,11 +4,12 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 #url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
-#url = "https://docs.google.com/spreadsheets/d/1kB0oWRD6vOnNHzilJdofS6AF1u-hBTHYPP-ELi0GADo/edit#gid=258115823"
-url = "https://docs.google.com/spreadsheets/d/1kB0oWRD6vOnNHzilJdofS6AF1u-hBTHYPP-ELi0GADo/edit?usp=sharing"
+url = "https://docs.google.com/spreadsheets/d/1kB0oWRD6vOnNHzilJdofS6AF1u-hBTHYPP-ELi0GADo/edit#gid=258115823"
+#url = "https://docs.google.com/spreadsheets/d/1kB0oWRD6vOnNHzilJdofS6AF1u-hBTHYPP-ELi0GADo/edit?usp=sharing"
 
 #conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url,worksheet="258115823")
+#data = conn.read(spreadsheet=url, worksheet="METRICAS", usecols=list(range(15)))
+data = conn.read(spreadsheet=url, worksheet="258115823", usecols=list(range(5))
 st.dataframe(data)
