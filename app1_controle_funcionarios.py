f207f8a0440353f1770c69810a8c963439bf5e6d
gb-garc <165524994+gb-garc@users.noreply.github.com>
2024-03-31 10:54:43 -0300

    Update app1_controle_funcionarios.py

--- app1_controle_funcionarios.py
+++ app1_controle_funcionarios.py
@@ -1,27 +1,9 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-
 url = "https://docs.google.com/spreadsheets/d/1GswNpQuhhc6udp59clV5s6dDnBfFF91rofaRbMsDdT0/edit#gid=704841034"
-
-@st.cache(allow_output_mutation=True)
-def get_gsheets_connection():
-    # Initialize GSheetsConnection with credentials or API token if necessary
-    # For example:
-    # return GSheetsConnection(api_key="your_api_key_here")
-    return GSheetsConnection()
-
-conn = get_gsheets_connection()
+conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(spreadsheet=url, worksheet="METRICAS", usecols=list(range(15)))
 st.dataframe(df)
-
-
-
-#import streamlit as st
-#from streamlit_gsheets import GSheetsConnection
-#url = "https://docs.google.com/spreadsheets/d/1GswNpQuhhc6udp59clV5s6dDnBfFF91rofaRbMsDdT0/edit#gid=704841034"
-#conn = st.connection("gsheets", type=GSheetsConnection)
-#df = conn.read(spreadsheet=url, worksheet="METRICAS", usecols=list(range(15)))
-#st.dataframe(df)
 
 
 #url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
