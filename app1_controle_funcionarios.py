db72d80ae6cff768d29ff30dfd4d91382a0b2d45
gb-garc <165524994+gb-garc@users.noreply.github.com>
2024-04-01 22:39:41 -0300

    Update app1_controle_funcionarios.py

--- app1_controle_funcionarios.py
+++ app1_controle_funcionarios.py
@@ -66,7 +66,7 @@ col1, col2 = st.columns(2)
 col_filtro = col1.selectbox('Selecione a coluna', [c for c in colunas if c not in ['OBRA SOLICIT:']])
 valor_filtro = col2.selectbox('Selecione o valor', list(df[col_filtro].unique()))
 
-colunas_selecionadas = st.multiselect('Selecione as colunas:', colunas[:11], ['TIPO', 'SOLICITANTE', 'SOLICITADO EM:', 'SITUACAO'])
+colunas_selecionadas = st.multiselect('Selecione as colunas:', colunas[:12], ['TIPO', 'SOLICITANTE', 'SOLICITADO EM:', 'SITUACAO'])
 st.divider()
 if status_filtrar:
     st.cache_data.clear()
