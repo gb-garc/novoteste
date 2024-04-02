15c1dd83734a9d684eda9c07d94fbea7ce1d4605
gb-garc <165524994+gb-garc@users.noreply.github.com>
2024-04-01 21:28:13 -0300

    Update app1_controle_funcionarios.py

--- app1_controle_funcionarios.py
+++ app1_controle_funcionarios.py
@@ -24,18 +24,19 @@ st.divider()
 
 
 # CRIAR BOTÃO QUE ABRE O FORMULÁRIO
+st.markdown('# Formulário')
 url2="https://docs.google.com/forms/d/e/1FAIpQLSfJBAV_3q-3EN1R0qmIMYXrJHydjG2l0YzeZGn03qw5BsxojQ/viewform"
 st.link_button("Clique aqui para abrir um formulário de todas as solicitações", url2)
 st.divider()
 
 # CRIAR BOTÕES PARA PESQUISA DE SOLICITAÇÃO ESPECÍFICA 
+st.markdown('# Pesquisar solicitação')
 colunas = list(df.columns)
 unique_index_values = df.index.unique().tolist()
 col4, col5 = st.columns(2)
 valor_filtro2=col4.selectbox('Selecione uma solicitação para consultar os dados detalhados', unique_index_values)
 status_filtrar2 = col5.button('Ver detalhes')
 status_limpar2 = col5.button('Limpar pesquisa')
-#st.divider()
 
 if status_filtrar2:
     texto1=valor_filtro2
@@ -49,6 +50,7 @@ if status_limpar2:
 st.divider()
 
 # CRIAR BOTÕES E MULTISELECT PARA EDITAR TABELA COM LISTA COMPLETA
+st.markdown('# Lista Completa de Solicitações')
 col6, col7, col8=st.columns(3)
 status_limpar = col6.button(' Ver  tabela  completa ')
 status_filtrar = col7.button('Ver tabela com filtros')
@@ -60,17 +62,12 @@ valor_filtro = col2.selectbox('Selecione o valor', list(df[col_filtro].unique())
 
 colunas_selecionadas = st.multiselect('Selecione as colunas:', colunas, ['TIPO', 'SOLICITANTE', 'SOLICITADO EM:', 'SITUACAO', 'ORDEM'])
 st.divider()
-
 if status_filtrar:
-    st.markdown('# Lista Completa de Solicitações')
     st.dataframe(df.loc[df[col_filtro] == valor_filtro, colunas_selecionadas], height=800,width=800)
 elif status_limpar:
-    st.markdown('# Lista Completa de Solicitações')
     st.dataframe(df[colunas_selecionadas],height=800,width=800)
 elif status_ocultar:
     st.write("")
-    st.write("")
 else:
-    st.write("")
     st.write("")
     
